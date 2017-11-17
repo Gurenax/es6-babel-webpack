c3dc2cf95a3ade0ad7df7b95d851c192aac8a7fb
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-18 00:00:31 +1100

    Update modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   entry: ['./app/index.js'],
   output: {
@@ -17,5 +19,11 @@ module.exports = {
     port: 3000,
     contentBase: './build/',
     inline: true
-  }
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env', // Path to .env file (this is the default) 
+      safe: false // load .env.example (defaults to "false" which does not use dotenv-safe) 
+    })
+  ]
 }
